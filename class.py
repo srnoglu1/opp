cf1044f9ba0fc6844d7618af4f72150cc605caa1
emirhan sirinoglu <srnoglu98@gmail.com>
2021-05-03 18:54:12 +0300

    object-orient-class

---
+++ class.py
@@ -0,0 +1,27 @@
+# Class
+
+class studient:
+    # method
+    # attribute
+    pass
+
+# Object, Instance
+
+studient1 = studient()
+studient2 = studient()
+
+# print(type(studient))
+# print(studient1, studient2)
+
+class Product:
+    pass
+
+p1 = Product() # Samsung S10
+p2 = Product() # IPhone 12
+p3 = Product() # IPhone 12 Pro
+
+products = [p1,p2,p3]
+
+for p in products:
+    print(p)
+    print(type(p))
