d9911ab856f7c1d31376318b95b3aa6584190e01
emirhan sirinoglu <srnoglu98@gmail.com>
2021-05-13 22:05:45 +0300

    class-attributes

---
+++ class-attr.py
@@ -0,0 +1,32 @@
+class User:
+
+    active_users = 0
+
+    def __init__(self,first,last,age):
+        self.first = first
+        self.last = last
+        self.age = age
+        User.active_users += 1
+
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.full_name()} exited the application."
+
+
+userA = User("Sadık","KARTAL",12)
+userB = User("Osman","AKTAN",15)
+userC = User("Hasan","SARAL",24)
+
+print(User.active_users)
+print(userA.logout())
+print(User.active_users)
+
+
+
+
+
+
+# print(userA.full_name())
